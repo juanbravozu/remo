8517138f0c020b96f20de5e4efa4dd34bef89c02
Juan Bravo <juanda.bravo98@gmail.com>
2021-06-16 23:41:53 -0500

    Added scheduler

---
+++ src/components/Appointment.tsx
@@ -0,0 +1,27 @@
+import { Appointments } from "@devexpress/dx-react-scheduler-material-ui";
+import React, { FC } from "react";
+
+const Appointment:FC<any> = ({children, style, data, resources, ...props}) => {
+
+    return (
+        <Appointments.Appointment 
+            {...props} 
+            style={{
+                ...style,
+                backgroundColor: '#FFFFFF',
+                borderLeft: '8px solid #A65290',
+                width: '96%',
+                left: '2%',
+                boxShadow: '2px 5px 15px -5px rgb(0 0 0 / 20%)',
+                borderRadius: '1rem'
+            }}
+            draggable={true}
+            data={data}
+            resources={resources}
+        >
+            {children}
+        </Appointments.Appointment>
+    )
+}
+
+export default Appointment;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,39 +1,50 @@
-import React, { FC, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { db, userData } from '../utils/firebase';
+import React, { FC, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { db } from '../utils/firebase';
 import { useAuth } from '../contexts/AuthContext'
 import { Button, Drawer, IconButton } from "@material-ui/core";
 import { ExitToApp } from "@material-ui/icons";
 import MenuIcon from "@material-ui/icons/Menu";
-import { setGlobalUser, IAvailability, IUser, parseDaysToWeek } from "../utils/algorithm";
+/* import { setGlobalUser, IAvailability, IUser, parseDaysToWeek } from "../utils/algorithm"; */
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import { Appointments, AppointmentTooltip, DayView, Scheduler, WeekView } from "@devexpress/dx-react-scheduler-material-ui";
+import Appointment from "../components/Appointment";
+
+const taskExampleData = [{startDate: '2021-06-16T09:45', endDate: '2021-06-16T11:45', title: 'Holiwi we'}]
 
 const Dashboard:FC = () => {
 
     const [ openMenu, setOpenMenu ] = useState<boolean>(false);
     const { currentUser } = useAuth()!;
     const [ name, setName ] = useState<string>('');
+    const [ tasks, setTasks ] = useState();
     const { logout } = useAuth()!;
     const history = useHistory();
+    const [ currentDate, setCurrentDate ] = useState(new Date());
 
-    db.collection('users').doc(currentUser.uid).get()
-    .then(data => {
-        if(!(data.data()!.profile)) return history.push('/signup');
-        const myUser:IUser = {
-            name: data.data()!.name as string,
-            lunch: data.data()!.lunch as number,
-            uid: data.data()!.uid as string,
-            email: data.data()!.email as string,
-            workday: data.data()!.workday as Array<number>,
-            profile: data.data()!.profile as number,
-            satisfaction: data.data()!.satisfaction as number,
-            nap: data.data()!.nap as number,
-            tasks: data.data()!.tasks
-        }
+    useEffect(() => {
+        db.collection('users').doc(currentUser.uid).get()
+        .then(data => {
+            if(!(data.data()!.profile)) return history.push('/signup');
+            /* const myUser:IUser = {
+                name: data.data()!.name as string,
+                lunch: data.data()!.lunch as number,
+                uid: data.data()!.uid as string,
+                email: data.data()!.email as string,
+                workday: data.data()!.workday as Array<number>,
+                profile: data.data()!.profile as number,
+                satisfaction: data.data()!.satisfaction as number,
+                nap: data.data()!.nap as number,
+                tasks: data.data()!.tasks
+            }
 
-        parseDaysToWeek([data.data()!.day1, data.data()!.day2, data.data()!.day3, data.data()!.day4, data.data()!.day5, data.data()!.day6, data.data()!.day0] as Array<Array<IAvailability>>, myUser)
-        setGlobalUser(myUser);
-        setName(myUser.name);
-    });
+            parseDaysToWeek([data.data()!.day1, data.data()!.day2, data.data()!.day3, data.data()!.day4, data.data()!.day5, data.data()!.day6, data.data()!.day0] as Array<Array<IAvailability>>, myUser)
+            setGlobalUser(myUser); */
+            setName(data.data()!.name as string);
+            setTasks(data.data()!.tasks);
+        });
+    }, [])
+    
 
     function handleLogOut() {
         logout()
@@ -41,6 +52,8 @@ const Dashboard:FC = () => {
             history.push('/login');
         })
     }
+
+    console.log('Se está renderizando');
 
     return (
     <React.Fragment>
@@ -51,7 +64,13 @@ const Dashboard:FC = () => {
         </header>
 
         <main>
-            <h1>Hola {name && name}</h1>
+            <h1>Hola {name.split(' ')[0]}</h1>
+            <Scheduler data={taskExampleData} firstDayOfWeek={1}>
+                <ViewState currentDate={currentDate}/>
+                <WeekView startDayHour={5} endDayHour={24} cellDuration={60}/>
+                <Appointments appointmentComponent={Appointment}/>
+                <AppointmentTooltip />
+            </Scheduler>
         </main>
         <Drawer PaperProps={{classes: {root: "menu"}}} anchor="left" open={openMenu} onClose={() => setOpenMenu(false)} style={openMenu ? {} : {transition: 'opacity 0.3s ease-out', opacity: 0, pointerEvents: 'none'}}>
             <div>
